a686b2cffa43f379a546152a68da302bab4cd06d
Moheed <syedmoheedulhassan@gmail.com>
2020-08-16 14:52:48 +0300

    ok

--- personal_portfolio/wsgi.py
+++ personal_portfolio/wsgi.py
@@ -10,9 +10,6 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
-
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'personal_portfolio.settings')
 
 application = get_wsgi_application()
