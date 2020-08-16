fa1f336722b17026235ea7135c9bfbfd8701afee
Moheed <syedmoheedulhassan@gmail.com>
2020-08-16 11:21:11 +0300

    additional files and changes for Heroku

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -132,6 +132,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT=os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = 'media/'
 
